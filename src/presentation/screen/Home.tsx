17d927797045ec7591268bd9e649536088f8678c
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2025-05-16 16:05:36 +0700

    edit home screen

--- src/presentation/screen/Home.tsx
+++ src/presentation/screen/Home.tsx
@@ -1,55 +1,127 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   Text,
   View,
   StyleSheet,
-  Button,
   NativeModules,
-  TextInput,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const {width, height} = Dimensions.get('window');
+const buttonSize = Math.min(width, height) / 4; // Adjust button size based on screen dimensions
+const textSize = buttonSize * 0.5; // Adjust text size based on button size
 const HomeScreen = () => {
   const {VoipModule} = NativeModules;
 
   const [tel, setTel] = React.useState<string>('');
-  const [isCalling, setIsCalling] = React.useState<boolean>(false);
+  const [formatTel, setFormatTel] = React.useState<string>('');
+  // const [isCalling, setIsCalling] = React.useState<boolean>(false);
+
+  const keyPads: {key: string; value: string}[][] = [
+    [
+      {key: '1', value: '1'},
+      {key: '2', value: '2'},
+      {key: '3', value: '3'},
+    ],
+    [
+      {key: '4', value: '4'},
+      {key: '5', value: '5'},
+      {key: '6', value: '6'},
+    ],
+    [
+      {key: '7', value: '7'},
+      {key: '8', value: '8'},
+      {key: '9', value: '9'},
+    ],
+    [
+      {key: '*', value: '*'},
+      {key: 'call', value: 'call'},
+      {key: '#', value: '#'},
+    ],
+  ];
+
+  useEffect(() => {
+    if (tel.length <= 3) {
+      setFormatTel(tel);
+    } else {
+      const preTel = tel.slice(0, 3);
+      const postTel = tel.slice(3);
+      const merheTel = preTel + '-' + postTel;
+      setFormatTel(merheTel);
+    }
+  }, [tel]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textWhite}>Home Screen</Text>
-      <Button
-        title="Test Voip Native Module"
-        onPress={() => {
-          VoipModule.createVoipEvent('John', '0987654321');
-        }}
-      />
-      <TextInput
-        placeholder="Enter your name"
-        onChangeText={(text: React.SetStateAction<string>) => {
-          setTel(text);
-        }}
-        style={styles.textInput}
-      />
+      <View style={styles.rowContent}>
+        {tel.length > 0 && (
+          <View style={styles.alignStart}>
+            <TouchableOpacity
+              onPress={() => {
+                setTel('');
+              }}>
+              <Icon name="backspace" size={32} color="green" />/
+            </TouchableOpacity>
+          </View>
+        )}
+        <View style={[styles.flexArea, styles.alignCenter]}>
+          <Text style={styles.fontTel}>{formatTel}</Text>
+        </View>
+        {tel.length > 0 && (
+          <View style={styles.alignEnd}>
+            <TouchableOpacity
+              onPress={() => {
+                if (tel.length === 0) {
+                  return;
+                }
+                setTel(tel.slice(0, -1));
+              }}>
+              <Icon name="backspace" size={32} color="green" />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
 
-      <Button
-        title="Call"
-        onPress={() => {
-          VoipModule.callVoip(tel);
-          setIsCalling(true);
-        }}
-        disabled={tel.length === 0}
-        color="blue"
-      />
-      <Button
-        title="End Call"
-        onPress={() => {
-          VoipModule.endCall();
-          setIsCalling(false);
-          setTel('');
-        }}
-        color="red"
-        disabled={!isCalling}
-      />
+      <View>
+        {keyPads.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.rowContent}>
+            {row.map(keyPad => (
+              <KeypadButton
+                key={keyPad.key}
+                keyPad={keyPad}
+                onPress={() => {
+                  if (tel.length === 10) return;
+                  setTel(prevTel => prevTel + keyPad.value);
+                }}
+              />
+            ))}
+          </View>
+        ))}
+      </View>
     </View>
+  );
+};
+
+const KeypadButton = ({
+  keyPad,
+  onPress,
+}: {
+  keyPad: {key: string; value: string};
+  onPress: () => void;
+}) => {
+  const isCall = keyPad.key === 'call';
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      {isCall ? (
+        <Icon name="backspace" size={32} color="green" />
+      ) : (
+        <Text style={styles.buttonText}>{keyPad.value}</Text>
+      )}
+    </TouchableOpacity>
   );
 };
 
@@ -58,6 +130,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
   },
   textWhite: {color: 'white'},
   textInput: {
@@ -67,7 +140,37 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   button: {
-    marginTop: 10,
+    padding: 20,
+    margin: 5,
+    backgroundColor: '#ddd',
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: 60,
+    width: buttonSize,
+  },
+  buttonText: {
+    fontSize: textSize,
+    color: '#000',
+  },
+  rowContent: {
+    flexDirection: 'row',
+  },
+  fontTel: {
+    fontSize: textSize,
+    color: '#000',
+  },
+  flexArea: {
+    flex: 1,
+  },
+  alignStart: {
+    alignItems: 'flex-start',
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  alignEnd: {
+    alignItems: 'flex-end',
   },
 });
 
